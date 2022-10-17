666caeec2719567cd0869384375bf2c930480d7b
Jacob I  Perez <jacobperez91@gmail.com>
2022-10-17 08:24:23 -1000

    outlets added

---
+++ Rick&MortyMVVM/Rick&MortyMVVM/CharacterTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CharacterTableViewCell.swift
+//  Rick&MortyMVVM
+//
+//  Created by Jacob Perez on 10/17/22.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
